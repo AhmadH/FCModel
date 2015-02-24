6d756e1eb7003eabd5b9d0068082e6f66d35ed81
Ahmad Alhashemi <ahmad@ahmadh.com>
2015-02-23 19:13:02 -0500

    Ported the Color model to Swift

---
+++ example/FCModelTest/Color.swift
@@ -0,0 +1,31 @@
+//
+//  Color.swift
+//  FCModelTest
+//
+//  Created by Ahmad Alhashemi on 2015-02-23.
+//  Copyright (c) 2015 Marco Arment. All rights reserved.
+//
+
+import Foundation
+
+func UIColorFromHex(rgbValue : UInt32) -> UIColor {
+    let r = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+    let g = CGFloat((rgbValue & 0xFF00) >> 8) / 255.0
+    let b  = CGFloat(rgbValue & 0xFF) / 255.0
+    return UIColor(red: r, green: g, blue: b, alpha: 1.0)
+}
+
+@objc(Color)
+class Color : FCModel {
+    var name : String = "" // database primary key (doesn't have to be an integer)
+    var hex : String = "" // database column
+    
+    // not a database column
+    var colorValue : UIColor {
+        get {
+            var hexColor : UInt32 = 0
+            NSScanner(string: self.hex).scanHexInt(&hexColor)
+            return UIColorFromHex(hexColor)
+        }
+    }
+}
